604c7aae64cf42da14f666f18e3c21a1adc5a1af
Rakesh Gorkhanath Khandagale <khandagale87.rakesh@gmail.com>
2021-02-18 16:17:47 -0500

    model component complete. business logic complete

---
+++ swift-app-windowShopper/swift-app-windowShopper/Model/Wage.swift
@@ -0,0 +1,8 @@
+//
+//  Wage.swift
+//  swift-app-windowShopper
+//
+//  Created by Rakesh Khandagale on 2/18/21.
+//
+
+import Foundation
---
+++ swift-app-windowShopper/swift-app-windowShopper/View/CurrencyTextField.swift
@@ -0,0 +1,8 @@
+//
+//  CurrencyTextField.swift
+//  swift-app-windowShopper
+//
+//  Created by Rakesh Khandagale on 2/18/21.
+//
+
+import Foundation
